eb754886432504e5519fe119274a9c9d0ca06888
Nick Sacco <nicksacco10@gmail.com>
2023-07-22 17:59:02 -0700

    test: merchant service tests for all merchants

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,9 +1,7 @@
 class MerchantFacade
   def merchants
     service = MerchantService.new
-
     json = service.all_merchants
-
     @merchants = json[:data].map do |merch_data|
       Merchant.new(merch_data)
     end
---
+++ spec/services/merchant_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe MerchantService do
+  describe 'class methods' do
+    describe '#all_merchants' do
+      it 'returns all merchants' do
+        merchant = MerchantService.new.all_merchants
+        expect(merchant).to be_a Hash
+        expect(merchant[:data]).to be_an Array
+
+        merch_data = merchant[:data].first
+        expect(merch_data[:attributes]).to have_key :name
+        expect(merch_data[:attributes][:name]).to be_a String
+      end
+    end
+  end
+end
